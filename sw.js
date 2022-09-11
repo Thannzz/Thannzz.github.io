048d7750e12a2879b0cdbb754dc3beab9a26efd0
Thannzz <thanigai128@gmail.com>
2022-09-11 19:09:11 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-44571683af8f977102d7.js"
+    "url": "webpack-runtime-4a219968d26709e53d10.js"
   },
   {
     "url": "framework-d13cd8053ca35f993551.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f9b986a922b6f1694e3657c385ca38ff"
+    "revision": "404f7b37770713f7016006c2ff951c2c"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-4f2107f39f93373060b1.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "3c72888a9e4a353e702157d8b9259ff1"
+    "revision": "c230871535239b566ca4120684ebfd9a"
   },
   {
     "url": "polyfill-77bb30979aec2e89586e.js"
